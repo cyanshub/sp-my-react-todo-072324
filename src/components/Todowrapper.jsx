c3fe0d512155af4146093b96b6e3b5c4a791ce86
travel15740 <travel15740@gmail.com>
2024-07-22 22:37:03 +0800

    在 TodoWrapper 組件, 把陣列資料用 props 傳入 Todo 組件 (關閉props的 eslint)

--- src/components/Todowrapper.jsx
+++ src/components/Todowrapper.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import CreateForm from "./CreateForm";
 import Todo from "./Todo";
 
 function TodoWrapper() {
+
+  // 通常會使用資料庫的資料, 這邊只是示範資料
+  const [todos, setTodos] = useState([
+    {content: '打掃廁所', id: Math.random()},
+    {content: '寫作業', id:Math.random()}
+  ])
+
   return (
     <>
       <div className="wrapper">
         <h1>待辦事項</h1>
         <CreateForm />
-        <Todo />
+        {todos.map( todo => {
+          return <Todo key={todo.id} todo={todo.content} />
+        } )}
+        
       </div>
     </>
   );
