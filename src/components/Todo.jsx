5ca836c340e77cda2cf46f9e267bacdffe8b4926
travel15740 <travel15740@gmail.com>
2024-07-22 23:22:31 +0800

    在 Todo 組件加入圖標套件

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,13 @@
+import { MdDelete, MdEdit } from "react-icons/md";
+
 function Todo({ todo }) {
   return (
     <div className="todo">
       <p>{todo}</p>
+      <div>
+        <MdEdit style={{ cursor: "pointer" }} />
+        <MdDelete style={{ cursor: "pointer", marginLeft: '5px' }} />
+      </div>
     </div>
   );
 }
