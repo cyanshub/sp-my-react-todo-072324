818fe3a4cf6b8cca2af365e7d01d61291311df8c
travel15740 <travel15740@gmail.com>
2024-07-22 22:42:29 +0800

    建立一個 addTodo 函數(觸發set方法與展開運算子), 並傳入CreateForm組件

--- src/components/Todowrapper.jsx
+++ src/components/Todowrapper.jsx
@@ -3,22 +3,25 @@ import CreateForm from "./CreateForm";
 import Todo from "./Todo";
 
 function TodoWrapper() {
-
   // 通常會使用資料庫的資料, 這邊只是示範資料
   const [todos, setTodos] = useState([
-    {content: '打掃廁所', id: Math.random()},
-    {content: '寫作業', id:Math.random()}
-  ])
+    { content: "打掃廁所", id: Math.random() },
+    { content: "寫作業", id: Math.random() },
+  ]);
+
+  const addTodo = (content) => {
+    // 使用展開運算子結合set方法, 加入新的陣列資料
+    setTodos([...todos, { content, id: Math.random() }]);
+  };
 
   return (
     <>
       <div className="wrapper">
         <h1>待辦事項</h1>
-        <CreateForm />
-        {todos.map( todo => {
-          return <Todo key={todo.id} todo={todo.content} />
-        } )}
-        
+        <CreateForm addTodo={addTodo} />
+        {todos.map((todo) => {
+          return <Todo key={todo.id} todo={todo.content} />;
+        })}
       </div>
     </>
   );
